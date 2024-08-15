e584ce0517dcf3ed5a037b4ca3ba7a7f88e16688
rutujamane6526 <rutujamane6526@gmail.com>
2024-08-15 13:00:30 +0530

    React Step-Count Project first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+function App()
+{
+  const [step, setStep] = useState(1); 
+  const [count, setCount] = useState(0);
+  return(
+    <>
+      <div className="inside-container">
+        <button onClick={ () => {
+          setStep(step - 1);
+        }}>-</button>
+        <h2>Step: {step} </h2>
+        <button onClick={ () => {
+          if (step < 10) setStep(step + 1);
+        }}>+</button>
+      </div>
+
+      <div className="inside-container">
+        <button onClick={ () => {
+         if(step > 0) setCount(count - step);
+        }}>-</button>
+        <h2>Count: {count}</h2>
+        <button onClick={ 
+          function(){
+            setCount(count + step);
+          }}>+</button>
+      </div>
+
+      <div className="result">
+        <p> 
+          Your count is <b>{count}</b> and step is <b>{step}</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
